67bea53b091620daf56be3b4d5094146132c803d
Gabriel Matheus <166963853+gabriel-mat@users.noreply.github.com>
2025-07-21 20:57:02 -0300

    Implementação do Prim Jarnik (MST)

---
+++ prim-jarnik.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <limits.h>
+
+int *prim_jarnik(grafo* g){
+    fila f;
+    int *keys = (int *)malloc(g->V * sizeof(int));
+    int *visitados = (int *)calloc(g->V, sizeof(int));
+
+    for (int i = 0; i < g->num_vertices; i++)
+        chaves[i] = INT_MAX;
+
+    inserir_fila(&fila, origem, 0);
+    keys[origem] = 0;
+
+    while(!vazia(fila)){
+        int min = extrai_min(&f);
+        vis[min] = 1;
+
+        for(link n = g->adj[min]; n != NULL; n = n->next){
+            if(!vis[n->data] && keys[n->data] > n->peso){
+                keys[n->data] = n->peso;
+                inserir_fila(&fila, n->data, n->peso);
+            }
+        }
+    }
+
+    return keys;
+}
