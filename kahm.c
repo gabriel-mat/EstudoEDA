1ed27f673237c22d7d2c7597896c4fd2455325d7
Gabriel Matheus <166963853+gabriel-mat@users.noreply.github.com>
2025-07-22 06:52:12 -0300

    Implementação do algoritmo de Kahm (Ordenação Topológica)

---
+++ kahm.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+int *kahm(grafo *g){
+    int u, counter;
+    int *graus = (int *)calloc(g->V , sizeof(int));
+    int *ordem = (int *)malloc(g->V * sizeof(int));
+
+    for(int i = 0; i < g->V; i++)
+        for(link l = g->adj[i]; l; l = l->next)
+            graus[l->v]++;
+
+    fila f;
+    inicializa_fila(&f);
+
+    for(int i = 0; i < g->V; i++)
+        if(!graus[i])
+            insere_fila(&f, i);
+
+    counter = 0;
+
+    while(!fila_vazia(&f)){
+        u = remove_fila(&f);
+        ordem[counter++] = u;
+
+        for(link l = g->adj[u]; l; l = l-> next){
+            graus[l->v]--;
+
+            if(!graus[l->v])
+                insere_fila(&f, l->v);
+        }
+    }
+
+    return ordem;
+}
